104e9c22cf61df056c585091818581e02f9aa46c
Javis <javischen9548@foxmail.com>
2021-04-18 02:13:13 +0800

    u

--- src/api/usergroup-api.js
+++ src/api/usergroup-api.js
@@ -15,6 +15,8 @@ export const getUserGroup = (params) => get({url: api.usergroup, params})
 
 export const getAllUserGroups = (data) => get({url: api.usergroupsAll, data})
 
+export const deleteUserGroup = (params) => del({url: api.usergroup, params})
+
 export const getUserGroupsTree = (data) => post({url: api.usergroupsTree, data})
 
 export const addUserGroup = (data) => post({url: api.usergroup, data})
--- src/views/permission/usergroup/PermissionUserGroup.vue
+++ src/views/permission/usergroup/PermissionUserGroup.vue
@@ -1,104 +1,103 @@
 <template>
-    <a-card :bordered="false">
-      <!--          <div style="  background: #00a0e9;">123</div>-->
-<!--      <div class="table-page-search-wrapper">-->
-<!--        <a-form layout="inline">-->
-<!--          <a-row :gutter="48">-->
-<!--            <a-col :md="8" :sm="24">-->
-<!--              <a-form-item label="用户组名称">-->
-<!--                <a-input v-model="queryParam.name" placeholder=""/>-->
-<!--              </a-form-item>-->
-<!--            </a-col>-->
-<!--            <a-col :md="8" :sm="24">-->
-<!--              <a-form-item label="使用状态">-->
-<!--                <a-select v-model="queryParam.status" placeholder="请选择" :default-value="0"-->
-<!--                          @change="handleQueryStatusChange">-->
-<!--                  <a-select-option v-for="(value, key) in routeStatusDictionary"-->
-<!--                                   :key="key"-->
-<!--                                   :value="key">-->
-<!--                    {{ value }}-->
-<!--                  </a-select-option>-->
-<!--                </a-select>-->
-<!--              </a-form-item>-->
-<!--            </a-col>-->
-<!--            <a-col :md="!advanced && 8 || 24" :sm="24">-->
-<!--                                    <span class="table-page-search-submitButtons"-->
-<!--                                          :style="advanced && { float: 'right', overflow: 'hidden' } || {} ">-->
-<!--                                      <a-button type="primary" @click="loadTableData">查询</a-button>-->
-<!--                                      <a-button style="margin-left: 8px" @click="resetQueryParams">重置</a-button>-->
-<!--                                    </span>-->
-<!--            </a-col>-->
-<!--          </a-row>-->
-<!--        </a-form>-->
-<!--      </div>-->
+  <a-card :bordered="false">
+    <!--          <div style="  background: #00a0e9;">123</div>-->
+    <!--      <div class="table-page-search-wrapper">-->
+    <!--        <a-form layout="inline">-->
+    <!--          <a-row :gutter="48">-->
+    <!--            <a-col :md="8" :sm="24">-->
+    <!--              <a-form-item label="用户组名称">-->
+    <!--                <a-input v-model="queryParam.name" placeholder=""/>-->
+    <!--              </a-form-item>-->
+    <!--            </a-col>-->
+    <!--            <a-col :md="8" :sm="24">-->
+    <!--              <a-form-item label="使用状态">-->
+    <!--                <a-select v-model="queryParam.status" placeholder="请选择" :default-value="0"-->
+    <!--                          @change="handleQueryStatusChange">-->
+    <!--                  <a-select-option v-for="(value, key) in routeStatusDictionary"-->
+    <!--                                   :key="key"-->
+    <!--                                   :value="key">-->
+    <!--                    {{ value }}-->
+    <!--                  </a-select-option>-->
+    <!--                </a-select>-->
+    <!--              </a-form-item>-->
+    <!--            </a-col>-->
+    <!--            <a-col :md="!advanced && 8 || 24" :sm="24">-->
+    <!--                                    <span class="table-page-search-submitButtons"-->
+    <!--                                          :style="advanced && { float: 'right', overflow: 'hidden' } || {} ">-->
+    <!--                                      <a-button type="primary" @click="loadTableData">查询</a-button>-->
+    <!--                                      <a-button style="margin-left: 8px" @click="resetQueryParams">重置</a-button>-->
+    <!--                                    </span>-->
+    <!--            </a-col>-->
+    <!--          </a-row>-->
+    <!--        </a-form>-->
+    <!--      </div>-->
 
-      <div class="table-operator">
-        <a-button type="primary" icon="plus" @click="showUserForm">新建用户组</a-button>
-      </div>
+    <div class="table-operator">
+      <a-button type="primary" icon="plus" @click="showUserForm">新建用户组</a-button>
+    </div>
 
-      <a-table
-        v-if="userGroups && userGroups.length > 0"
-        bordered
-        :pagination="false"
-        :loading="tableLoading"
-        :defaultExpandAllRows="defaultExpandAllRows"
-        :expandRowByClick="false"
-        :size="'middle'"
-        :indent-size="15"
-        :data-source="userGroups"
-        :row-key="rowKey"
-        :columns="columns">
+    <a-table
+      v-if="userGroups && userGroups.length > 0"
+      bordered
+      :pagination="false"
+      :loading="tableLoading"
+      :defaultExpandAllRows="defaultExpandAllRows"
+      :expandRowByClick="false"
+      :size="'middle'"
+      :indent-size="15"
+      :data-source="userGroups"
+      :row-key="rowKey"
+      :columns="columns">
 
-        <template slot="roles" slot-scope="text, record">
-          <a-tag color="blue" v-for="item in record.roles" :key="item">{{ item }}</a-tag>
-        </template>
+      <template slot="roles" slot-scope="text, record">
+        <a-tag color="blue" v-for="item in record.roles" :key="item">{{ item }}</a-tag>
+      </template>
 
-        <template slot="inheritType" slot-scope="text, record">
-          {{ getInheritTypeDesc(record.inheritType) }}
-        </template>
+      <template slot="inheritType" slot-scope="text, record">
+        {{ getInheritTypeDesc(record.inheritType) }}
+      </template>
 
-        <template slot="type" slot-scope="text, record">
-          {{ getTypeDesc(record.type) }}
-        </template>
+      <template slot="type" slot-scope="text, record">
+        {{ getTypeDesc(record.type) }}
+      </template>
 
-        <template slot="status" slot-scope="text, record">
-          <a-dropdown :trigger="['click']">
-            <a-menu slot="overlay" @click="routeStatusChange($event, record)">
-              <a-menu-item v-for="(value, key) in routeStatusDictionary" :key="key">
-                {{ value }}
-              </a-menu-item>
-            </a-menu>
-            <a-button
-              :style="record.status === 1 ? {'background-color': '#52c41a',border: 'none', 'color': 'white'}: {}"
-              shape="round" size="small" :type="record.status !== 1 ? 'danger' : ''">
-              {{ getStatusDesc(record.status) }}
-              <a-icon type="down"/>
-            </a-button>
-          </a-dropdown>
-        </template>
+      <template slot="status" slot-scope="text, record">
+        <a-dropdown :trigger="['click']">
+          <a-menu slot="overlay" @click="routeStatusChange($event, record)">
+            <a-menu-item v-for="(value, key) in routeStatusDictionary" :key="key">
+              {{ value }}
+            </a-menu-item>
+          </a-menu>
+          <a-button
+            :style="record.status === 1 ? {'background-color': '#52c41a',border: 'none', 'color': 'white'}: {}"
+            shape="round" size="small" :type="record.status !== 1 ? 'danger' : ''">
+            {{ getStatusDesc(record.status) }}
+            <a-icon type="down"/>
+          </a-button>
+        </a-dropdown>
+      </template>
 
-        <template slot="action" slot-scope="text, record">
-          <k-tooltip-button title="编辑" @click="handleEdit(record)" icon="edit"/>&nbsp;
-          <k-tooltip-button title="删除" @click="handleDelete(record)" type="danger" icon="delete"/>
-        </template>
-      </a-table>
-      <a-empty v-else/>
+      <template slot="action" slot-scope="text, record">
+        <k-tooltip-button title="编辑" @click="handleEdit(record)" icon="edit"/>&nbsp;
+        <k-tooltip-button title="删除" @click="handleDelete(record)" type="danger" icon="delete"/>
+      </template>
+    </a-table>
+    <a-empty v-else/>
 
-      <!-- 创建路由信息表单-->
-      <permission-user-group-form ref="userGroupForm"
-                                  @success="handleFormOnSuccess"
-                                  @cancel="handleEditFormCancel"
-                                  :user-groups="userGroups"/>
+    <!-- 创建路由信息表单-->
+    <permission-user-group-form ref="userGroupForm"
+                                @success="handleFormOnSuccess"
+                                @cancel="handleEditFormCancel"
+                                :user-groups="userGroups"/>
 
-    </a-card>
-
+  </a-card>
 
 
 </template>
 
 <script>
 
-import {getUserGroups, getUserGroup} from '@/api/usergroup-api'
+import {getUserGroups, getUserGroup, deleteUserGroup} from '@/api/usergroup-api'
 import PermissionUserGroupForm from "./components/PermissionUserGroupForm";
 import PermissionUserGroupTree from "./components/PermissionUserGroupTree";
 import {filterNonChildren} from "@/utils/util";
@@ -235,8 +234,14 @@ export default {
     handleDelete(record) {
       this.$confirm({
         title: `提示`,
-        content: `确定要禁用[${record.name}]用户吗？`,
+        content: `确定要删除[${record.name}]用户组吗？`,
         onOk: async () => {
+          deleteUserGroup({id: record.id})
+            .then(resp => {
+              this.$message.success('删除成功');
+              this.loadTableData()
+            })
+            .catch((e) => e)
         }
       })
     },
