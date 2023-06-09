c3da463fc79acd286eb67a522565a038439d7ee9
Javis <javischen9548@foxmail.com>
2023-06-09 18:03:12 +0800

    u

--- src/api/iam/application-api.js
+++ src/api/iam/application-api.js
@@ -3,18 +3,20 @@ import Base from "./iam-base-api";
 
 const api = {
   applications: Base.serviceName + '/v1/applications',
-  application: Base.serviceName + '/v1/application',
+  applicationCreate: Base.serviceName + '/v1/application/create',
+  applicationUpdate: Base.serviceName + '/v1/application/update',
+  applicationDelete: Base.serviceName + '/v1/application/delete',
 }
 
 export default api
 
 export const getApplications = (data) => post({url: api.applications, data})
 
-export const addApplication = (data) => post({url: api.application, data})
+export const addApplication = (data) => post({url: api.applicationCreate, data})
 
-export const deleteApplication = (id) => del({url: `${api.application}/${id}`})
+export const deleteApplication = (id) => del({url: `${api.applicationDelete}/${id}`})
 
-export const updateApplication = (data) => put({url: api.application, data})
+export const updateApplication = (data) => put({url: api.applicationUpdate, data})
 
 
 
