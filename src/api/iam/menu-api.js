2e72cec57343efcf65d8e53f84d61a0fa84f5f2d
chengmo.cjw <javischen9548@foxmail.com>
2024-05-28 17:37:50 +0800

    u

---
+++ src/api/iam/menu-api.js
@@ -0,0 +1,33 @@
+import {post, get, del, put} from '@/utils/request'
+import Base from "./iam-base-api";
+
+const api = {
+  routesAll:Base.serviceName + '/v1/menus/all',
+  routes:Base.serviceName + '/v1/menus',
+  routeCreate:Base.serviceName + '/v1/menus',
+  routeUpdate:Base.serviceName + '/v1/menus',
+  route:Base.serviceName + '/v1/menus/details',
+  routeDelete:Base.serviceName + '/v1/menus',
+  routeStatus:Base.serviceName + '/v1/menus/status',
+  routeElements:Base.serviceName + '/v1/menus/elements',
+}
+
+export default api
+
+export const getRoutesTree = (data) => post({url: api.routesAll, data})
+
+export const getRoutes = (params) => get({url: api.routes, params})
+
+export const getRoute = (params) => get({url: `${api.route}`, params})
+
+export const addRoute = (data) => post({url: api.routeCreate, data})
+
+export const deleteRoute = (params) => del({url: api.routeDelete, params})
+
+export const updateRoute = (data) => put({url: api.routeUpdate, data})
+
+export const updateRouteStatus = (data) => put({url: api.routeStatus, data})
+
+export const getRouteElements = (params) => get({url: api.routeElements, params})
+
+
